bff9694df53805f382e219e8494541da1103e367
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-07-12 15:29:47 +0100

    feat:Implement a score counter

--- scripts/scrpt.js
+++ scripts/scrpt.js
@@ -4,10 +4,9 @@ function computerPlay(){
     let random = Math.floor(Math.random() * options.length)
     return(random, options[random])
 }
-
+let playerCounter=0; 
+let computerCounter=0;
 function playRound(playerSelection, computerSelection) {
-    let playerCounter=0; 
-    let computerCounter=0
     computerPlay()
   
     if (playerSelection==computerSelection){
@@ -15,41 +14,49 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection=="ROCK" & computerSelection=="PAPER"){
         alert("You lose!!! Paper beats Rock.")
+        computerCounter++
     }
     else if (playerSelection=="ROCK" & computerSelection=="SCISSORS"){
         alert("You win!!! Rock beats Scissors")
+        playerCounter++
     }
     else if(playerSelection=="PAPER" & computerSelection=="ROCK"){
         alert("You win!!! Paper beats Rock.")
+        playerCounter++
+
     }
     else if (playerSelection == "PAPER" & computerSelection == "SCISSORS"){
         alert("You lose!!! Scissors beats Paper.")
+        computerCounter++
     }
     else if (playerSelection == "SCISSORS" & computerSelection == "ROCK"){
         alert("You lose!!! Rock beats Scissors")
+        computerCounter++
     }
     else if (playerSelection == "SCISSORS"& computerSelection == "PAPER"){
         alert("You win!!! Scissors beats Paper.")
+        playerCounter++
     }
     else {
         alert("You have made a wrong entry")
     }
+  
 }
-// let playerSelection=prompt("ROCK, PAPER or SCISSORS?")
-// let computerSelection=computerPlay()
-// alert(computerSelection); 
-// alert(playerSelection.toUpperCase())
-// playRound(playerSelection.toUpperCase(), computerSelection)
 function game(){
     let i=0; 
-    while (i<=5){
+    while (i<3){
         let playerSelection=prompt("ROCK, PAPER or SCISSORS?")
         computerPlay()
-        let computerSelection=computerPlay()
+        let computerSelection=computerPlay() 
         playRound(playerSelection.toUpperCase(), computerSelection)
         i++
     }
+    alert('Your score:' +playerCounter+ ' Computer Score' +computerCounter);
 
+    if(playerCounter>computerCounter){alert("Congratulations!!! You've won the game")}
+    else if(playerCounter<computerCounter){alert("Sorry. You lose!!! ")}
+     else if (playerCounter==computerCounter){"It's a tie!!!"}
 }
+
 
 game()
